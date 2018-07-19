15e72bc1cc454ca941ff838e72f71f9086a8aa56
sronga <sr899@georgetown.edu>
2018-07-19 20:09:03 +0000

    automatically backed up by learn

--- lib/third_challenge.rb
+++ lib/third_challenge.rb
@@ -14,10 +14,10 @@ def third_challenge
    },
    :capulet => {
       :patriarch => {
-        :name =>
+        :name => "Lord Capulet", :age => "50"
         },
       :matriarch => {
-        #your key/value pairs here
+        :name => "Lady Capulet", :age => "51"
         },
       :heroine => {
         #your key/value pairs here
