f8731e8b0a1b3f44e464b4ca7b0151c3f18b5477
sronga <sr899@georgetown.edu>
2018-07-19 20:05:43 +0000

    automatically backed up by learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -4,7 +4,7 @@ def second_challenge
       :patriarch => {},
       :matriarch => {},
       :hero => {},
-      :friends => []
+      :hero_friends => []
       },
     :capulet => {
       #your key/value pairs here!
